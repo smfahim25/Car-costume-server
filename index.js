b483e556247c8141cf9ebe1a1c9251fba8623581
smfahim25 <smfahimhossen@gmail.com>
2022-05-27 21:05:49 +0600

    admin done

--- index.js
+++ index.js
@@ -151,13 +151,13 @@ async function run() {
             const users = await cursor.toArray()
             res.send(users)
         })
-        // app.get('/order', async (req, res) => {
+        app.get('/order', verifyJwt, async (req, res) => {
 
-        //     const query = {}
-        //     const cursor = orderCollection.find(query)
-        //     const orders = await cursor.toArray()
-        //     res.send(orders)
-        // })
+            const query = {}
+            const cursor = orderCollection.find(query)
+            const orders = await cursor.toArray()
+            res.send(orders)
+        })
 
 
         app.put('/user/:email', async (req, res) => {
